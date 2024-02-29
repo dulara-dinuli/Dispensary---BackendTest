7a8e1cfb6c24543e12730da3b6c734b10b6ebfaf
Dulara00 <dularadinuli@gmail.com>
2024-02-29 18:41:06 +0530

    Update

--- service/photoUpload.js
+++ service/photoUpload.js
@@ -4,7 +4,7 @@ const User = require('../model/User')
 
 var photoStorage = multer.diskStorage({
     destination: function(req,file,cb){
-        cb(null, 'uploads/')
+        cb(null, 'profilePictures/')
     },
     filename: function(req, file, cb){
         let ext = path.extname(file.originalname)
