bba6a90a1b63de377bbece97b161b3171a69fce4
Dulara00 <dularadinuli@gmail.com>
2024-03-01 09:08:55 +0530

    Add Authentication

--- controller/AuthenticationController.js
+++ controller/AuthenticationController.js
@@ -66,20 +66,11 @@ const login =(req, res, next) => {
                     })
                 }
                 if(result){
-                    let token = jwt.sign({name: user.firstName}, 'verySecretValue', {expiresIn: '1h'})
-                    
-                    const userDetails = [
-                        `First Name: ${user.firstName}`,
-                        `Last Name: ${user.lastName}`,
-                        `Mobile Number: ${user.mobileNumber}`,
-                        `Email: ${user.email}`,
-                        `Profile Picture Path: ${user.profilePicture}`
-                    ];
-                    
-                    const message = `Login Successful!\n${userDetails.join('\n')}\nToken: ${token}`;
-                    
-                    res.set('Content-Type', 'text/plain');
-                    res.send(message);
+                    let token = jwt.sign({name: user.firstName}, 'AzQ,PI)0(', {expiresIn: '1h'})
+                    res.json({
+                        message: 'Login Successful!',
+                        token
+                    })
                 }else{
                     res.json({
                         message: 'Password is incorrect!'
@@ -94,8 +85,33 @@ const login =(req, res, next) => {
     })
 }
 
+const showUser = (req, res, next) => {
+    var username = req.body.username
+    User.findOne({$or: [{email:username}]})
+    .then(user => {
+        const userDetails = [
+            `First Name: ${user.firstName}`,
+            `Last Name: ${user.lastName}`,
+            `Mobile Number: ${user.mobileNumber}`,
+            `Email: ${user.email}`,
+            `Profile Picture Path: ${user.profilePicture}`
+        ];
+        
+        const message = `Authorized User!\n${userDetails.join('\n')}`;
+        
+        res.set('Content-Type', 'text/plain');
+        res.send(message);
+    })
+    .catch(error => {
+        res.json({
+            message: 'An error Occured!'
+        })
+    })
+} 
+
 
 module.exports = {
     register,
-    login
+    login,
+    showUser
 }
--- router/authRoute.js
+++ router/authRoute.js
@@ -3,8 +3,10 @@ const router = express.Router()
 
 const AuthenticationController = require('../controller/AuthenticationController')
 const photoUpload = require('../service/photoUpload')
+const authenticate = require('../service/authenticate')
 
 router.post('/userRegister',photoUpload.single('profilePicture'), AuthenticationController.register)
 router.post('/userLogin', AuthenticationController.login)
+router.post('/showUser', authenticate, AuthenticationController.showUser)
 
 module.exports = router
